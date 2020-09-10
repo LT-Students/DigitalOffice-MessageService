6f4c9d4019c8e25c806491ae5bd74940b52bf4e4
lalag <lalagvanan@gmail.com>
2020-09-10 18:06:35 +0300

    Fixed directories

--- src/MessageService/Startup.cs
+++ src/MessageService/Startup.cs
@@ -44,7 +44,6 @@ namespace LT.DigitalOffice.MessageService
             services.AddTransient<IMessageRepository, MessageRepository>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseExceptionHandler(tempApp => tempApp.Run(CustomExceptionHandler.HandleCustomException));
