9fa0dede8573adf199f4f627c187eedc4306d8c6
Lena <59228285+lalagvana@users.noreply.github.com>
2020-09-18 22:07:35 +0300

    DbEmail table (#5)
    * Added DbEmail and DbEmailReciever tables
    
    * Fix the model
    
    * Add "required"
    
    * Update migration

---
+++ src/MessageService.Models.Db/DbEmail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LT.DigitalOffice.MessageService.Models.Db
+{
+    public class DbEmail
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid? SenderId { get; set; }
+        [Required]
+        public string Receiver { get; set; }
+        [Required]
+        public DateTime Time { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Body { get; set; }
+    }
+}
