2642ed216ba30a82f2520fd48822edd6b3060c4a
lalag <lalagvanan@gmail.com>
2020-09-28 22:31:10 +0300

    Update EmailTemplateRepositoryTests.cs

--- test/MessageService.Data.UnitTests/EmailTemplateRepositoryTests.cs
+++ test/MessageService.Data.UnitTests/EmailTemplateRepositoryTests.cs
@@ -16,6 +16,7 @@ namespace LT.DigitalOffice.MessageService.Data.UnitTests
 
         private DbEmailTemplate dbEmailTemplate;
         private Guid emailTemplateId;
+        private DbEmailTemplate dbEmailTemplateToAdd;
 
         [OneTimeSetUp]
         public void OneTimeSetUp()
@@ -32,6 +33,15 @@ namespace LT.DigitalOffice.MessageService.Data.UnitTests
         public void SetUp()
         {
             emailTemplateId = Guid.NewGuid();
+            dbEmailTemplateToAdd = new DbEmailTemplate
+            {
+                Id = Guid.NewGuid(),
+                Subject = "Subject",
+                Body = "Body",
+                AuthorId = Guid.NewGuid(),
+                IsActive = true,
+                CreatedAt = DateTime.UtcNow
+            };
             dbEmailTemplate = new DbEmailTemplate
             {
                 Id = emailTemplateId,
@@ -83,8 +93,8 @@ namespace LT.DigitalOffice.MessageService.Data.UnitTests
         [Test]
         public void ShouldAddEmailTemplateCorrectly()
         {
-            Assert.AreEqual(dbEmailTemplate.Id, repository.AddEmailTemplate(dbEmailTemplate));
-            Assert.AreEqual(dbEmailTemplate, provider.EmailTemplates.Find(dbEmailTemplate.Id));
+            Assert.AreEqual(dbEmailTemplateToAdd.Id, repository.AddEmailTemplate(dbEmailTemplateToAdd));
+            Assert.AreEqual(dbEmailTemplateToAdd, provider.EmailTemplates.Find(dbEmailTemplateToAdd.Id));
         }
     }
 }
