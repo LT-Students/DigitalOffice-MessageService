b5ff59735c1688d168b4876c077dbdeb305eb051
djino <malkinevgeniy437@gmail.com>
2020-10-01 21:03:17 +0300

    Added validator

---
+++ src/MessageService.Validation/EditEmailTemplateValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using LT.DigitalOffice.MessageService.Models.Dto;
+using System;
+
+namespace LT.DigitalOffice.MessageService.Validation
+{
+    public class EditEmailTemplateValidator : AbstractValidator<EditEmailTemplateRequest>
+    {
+        public EditEmailTemplateValidator()
+        {
+            RuleFor(et => et.Id)
+                .NotEmpty();
+
+            RuleFor(et => et.Subject)
+                .NotEmpty()
+                .MaximumLength(120)
+                .WithMessage("Subject too long");
+
+            RuleFor(et => et.Body)
+                .NotEmpty()
+                .MaximumLength(1000)
+                .WithMessage("Subject too long");
+        }
+    }
+}
