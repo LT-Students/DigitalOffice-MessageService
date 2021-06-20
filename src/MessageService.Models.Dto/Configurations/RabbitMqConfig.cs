57d9fcc063adb1faf3296c7ff4b8b0bd7d6f52ea
BelehovEgor <belehov.egor2001@yandex.ru>
2021-06-20 09:56:50 +0300

    remove extra injects requests

--- src/MessageService.Models.Dto/Configurations/RabbitMqConfig.cs
+++ src/MessageService.Models.Dto/Configurations/RabbitMqConfig.cs
@@ -1,19 +1,16 @@
 ﻿using LT.DigitalOffice.Kernel.Attributes;
 using LT.DigitalOffice.Kernel.Configurations;
 using LT.DigitalOffice.Models.Broker.Requests.File;
-using LT.DigitalOffice.Models.Broker.Requests.Message;
 
 namespace LT.DigitalOffice.MessageService.Models.Dto.Configurations
 {
     public class RabbitMqConfig : BaseRabbitMqConfig
     {
-        [AutoInjectRequest(typeof(ISendEmailRequest))]
         public string SendEmailEndpoint { get; set; }
 
         [AutoInjectRequest(typeof(IAddImageRequest))]
         public string AddImageEndpoint { get; set; }
 
-        [AutoInjectRequest(typeof(IGetEmailTemplateTagsRequest))]
         public string GetTempalateTagsEndpoint { get; set; }
     }
 }
