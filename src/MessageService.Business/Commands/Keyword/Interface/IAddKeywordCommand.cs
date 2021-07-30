af7ee51a0ae1f12a7db9de4d813fe71f85919f90
BelehovEgor <belehov.egor2001@yandex.ru>
2021-07-30 10:31:26 +0300

    add new controller

---
+++ src/MessageService.Business/Commands/Keyword/Interface/IAddKeywordCommand.cs
@@ -0,0 +1,13 @@
+﻿using LT.DigitalOffice.Kernel.Attributes;
+using LT.DigitalOffice.Kernel.Responses;
+using LT.DigitalOffice.MessageService.Models.Dto.Requests.ParseEntity;
+using System;
+
+namespace LT.DigitalOffice.MessageService.Business.Commands.ParseEntity.Interface
+{
+    [AutoInject]
+    public interface IAddKeywordCommand
+    {
+        OperationResultResponse<Guid> Execute(AddKeywordRequest request);
+    }
+}
---
+++ src/MessageService.Mappers/Db/Interfaces/IDbKeywordMapper.cs
@@ -0,0 +1,12 @@
+﻿using LT.DigitalOffice.Kernel.Attributes;
+using LT.DigitalOffice.MessageService.Models.Db;
+using LT.DigitalOffice.MessageService.Models.Dto.Requests.ParseEntity;
+
+namespace LT.DigitalOffice.MessageService.Mappers.Db.Interfaces
+{
+    [AutoInject]
+    public interface IDbKeywordMapper
+    {
+        DbKeyword Map(AddKeywordRequest request);
+    }
+}
