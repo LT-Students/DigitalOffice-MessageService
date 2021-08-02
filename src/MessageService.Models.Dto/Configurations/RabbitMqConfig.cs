0c89a2ba61b83b141a3c6f4cf0fef6a6978f8c07
BelehovEgor <61228367+BelehovEgor@users.noreply.github.com>
2021-08-02 12:13:04 +0300

    add keyword consumer (#40)
    * add new controller
    
    * fix type

--- src/MessageService.Models.Dto/Configurations/RabbitMqConfig.cs
+++ src/MessageService.Models.Dto/Configurations/RabbitMqConfig.cs
@@ -2,6 +2,7 @@
 using LT.DigitalOffice.Kernel.Configurations;
 using LT.DigitalOffice.Models.Broker.Requests.Company;
 using LT.DigitalOffice.Models.Broker.Requests.File;
+using System.Collections.Generic;
 
 namespace LT.DigitalOffice.MessageService.Models.Dto.Configurations
 {
@@ -10,6 +11,7 @@ namespace LT.DigitalOffice.MessageService.Models.Dto.Configurations
         public string CreateWorkspaceEndpoint { get; set; }
         public string SendEmailEndpoint { get; set; }
         public string UpdateSmtpCredentialsEndpoint { get; set; }
+        public Dictionary<string, string> FindUserParseEntitiesEndpoint { get; set; }
 
         [AutoInjectRequest(typeof(IGetSmtpCredentialsRequest))]
         public string GetSmtpCredentialsEndpoint { get; set; }
---
+++ src/MessageService.Models.Dto/Models/KeywordInfo.cs
@@ -0,0 +1,14 @@
+﻿using LT.DigitalOffice.MessageService.Models.Dto.Enums;
+using System;
+
+namespace LT.DigitalOffice.MessageService.Models.Dto.Models
+{
+    public record KeywordInfo
+    {
+        public Guid Id { get; set; }
+        public string Keyword { get; set; }
+        public ServiceName ServiceName { get; set; }
+        public string EntityName { get; set; }
+        public string PropertyName { get; set; }
+    }
+}
