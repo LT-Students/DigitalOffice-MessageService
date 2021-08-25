2e59fe6c991f546417011d01f6e090982cdb6f0e
BelehovEgor <belehov.egor2001@yandex.ru>
2021-08-25 10:41:24 +0300

    add editor

--- src/MessageService.Broker/Consumers/CreateWorkspaceConsumer.cs
+++ src/MessageService.Broker/Consumers/CreateWorkspaceConsumer.cs
@@ -1,40 +1,40 @@
-﻿using LT.DigitalOffice.Kernel.Broker;
+﻿using System.Threading.Tasks;
+using LT.DigitalOffice.Kernel.Broker;
 using LT.DigitalOffice.MessageService.Data.Interfaces;
 using LT.DigitalOffice.MessageService.Mappers.Db.Workspace.Interfaces;
 using LT.DigitalOffice.MessageService.Models.Db;
 using LT.DigitalOffice.Models.Broker.Requests.Message;
 using MassTransit;
-using System.Threading.Tasks;
 
 namespace LT.DigitalOffice.MessageService.Broker.Consumers
 {
-    public class CreateWorkspaceConsumer : IConsumer<ICreateWorkspaceRequest>
+  public class CreateWorkspaceConsumer : IConsumer<ICreateWorkspaceRequest>
+  {
+    private readonly IWorkspaceRepository _workspaceRepository;
+    private readonly IDbWorkspaceMapper _workspaceMapper;
+
+    private bool CreateWorkspace(ICreateWorkspaceRequest request)
     {
-        private readonly IWorkspaceRepository _workspaceRepository;
-        private readonly IDbWorkspaceMapper _workspaceMapper;
+      DbWorkspace newWorkspace = _workspaceMapper.Map(request);
 
-        private bool CreateWorkspace(ICreateWorkspaceRequest request)
-        {
-            DbWorkspace newWorkspace = _workspaceMapper.Map(request);
+      _workspaceRepository.Add(newWorkspace);
 
-            _workspaceRepository.Add(newWorkspace);
-
-            return true;
-        }
-
-        public CreateWorkspaceConsumer(
-            IWorkspaceRepository workspaceRepository,
-            IDbWorkspaceMapper workspaceMapper)
-        {
-            _workspaceRepository = workspaceRepository;
-            _workspaceMapper = workspaceMapper;
-        }
-
-        public async Task Consume(ConsumeContext<ICreateWorkspaceRequest> context)
-        {
-            var response = OperationResultWrapper.CreateResponse(CreateWorkspace, context.Message);
-
-            await context.RespondAsync<IOperationResult<bool>>(response);
-        }
+      return true;
     }
+
+    public CreateWorkspaceConsumer(
+        IWorkspaceRepository workspaceRepository,
+        IDbWorkspaceMapper workspaceMapper)
+    {
+      _workspaceRepository = workspaceRepository;
+      _workspaceMapper = workspaceMapper;
+    }
+
+    public async Task Consume(ConsumeContext<ICreateWorkspaceRequest> context)
+    {
+      var response = OperationResultWrapper.CreateResponse(CreateWorkspace, context.Message);
+
+      await context.RespondAsync<IOperationResult<bool>>(response);
+    }
+  }
 }
