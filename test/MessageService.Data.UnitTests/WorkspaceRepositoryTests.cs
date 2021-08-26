2fb9998c16317e82bdb4075f6e0902cdb2d8a3b2
BelehovEgor <belehov.egor2001@yandex.ru>
2021-08-26 12:50:12 +0300

    fix test

--- test/MessageService.Data.UnitTests/WorkspaceRepositoryTests.cs
+++ test/MessageService.Data.UnitTests/WorkspaceRepositoryTests.cs
@@ -1,119 +1,134 @@
-﻿using LT.DigitalOffice.MessageService.Data.Interfaces;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LT.DigitalOffice.Kernel.Exceptions.Models;
+using LT.DigitalOffice.MessageService.Data.Interfaces;
 using LT.DigitalOffice.MessageService.Data.Provider;
 using LT.DigitalOffice.MessageService.Data.Provider.MsSql.Ef;
 using LT.DigitalOffice.MessageService.Models.Db;
-using Microsoft.EntityFrameworkCore;
-using NUnit.Framework;
-using System;
 using LT.DigitalOffice.UnitTestKernel;
-using System.Linq;
-using LT.DigitalOffice.Kernel.Exceptions.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
 
 namespace LT.DigitalOffice.MessageService.Data.UnitTests
 {
-    internal class WorkspaceRepositoryTests
+  internal class WorkspaceRepositoryTests
+  {
+    private IDataProvider _provider;
+    private IWorkspaceRepository _repository;
+    private Mock<IHttpContextAccessor> _accessorMock;
+
+    private DbWorkspace _dbWorkspaceToAdd;
+    private DbWorkspace _dbWorkspaceInDb;
+
+    private Guid _userId = Guid.NewGuid();
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
     {
-        private IDataProvider _provider;
-        private IWorkspaceRepository _repository;
+      _dbWorkspaceToAdd = new DbWorkspace
+      {
+        Name = "Name",
+        Description = "Description",
+        IsActive = true,
+      };
 
-        private DbWorkspace _dbWorkspaceToAdd;
-        private DbWorkspace _dbWorkspaceInDb;
+      _dbWorkspaceInDb = new DbWorkspace
+      {
+        Id = Guid.NewGuid(),
+        Name = "Name",
+        IsActive = true
+      };
 
-        [OneTimeSetUp]
-        public void OneTimeSetUp()
-        {
-            _dbWorkspaceToAdd = new DbWorkspace
-            {
-                Name = "Name",
-                Description = "Description",
-                IsActive = true,
-            };
-
-            _dbWorkspaceInDb = new DbWorkspace
-            {
-                Id = Guid.NewGuid(),
-                Name = "Name",
-                IsActive = true
-            };
-
-            CreateMemoryDb();
-        }
-
-        public void CreateMemoryDb()
-        {
-            var dbOptions = new DbContextOptionsBuilder<MessageServiceDbContext>()
-                   .UseInMemoryDatabase(databaseName: "InMemoryDatabase")
-                   .Options;
-            _provider = new MessageServiceDbContext(dbOptions);
-
-            _repository = new WorkspaceRepository(_provider);
-        }
-
-        [SetUp]
-        public void SetUp()
-        {
-            _provider.Workspaces.Add(_dbWorkspaceInDb);
-            _provider.Save();
-        }
-
-        [TearDown]
-        public void CleanDb()
-        {
-            if (_provider.IsInMemory())
-            {
-                _provider.EnsureDeleted();
-            }
-        }
-
-        #region AddWorkspace
-
-        [Test]
-        public void ShouldAddWorkspaceCorrectly()
-        {
-            _dbWorkspaceToAdd.Id = Guid.NewGuid();
-
-            _repository.Add(_dbWorkspaceToAdd);
-
-            Assert.AreEqual(_dbWorkspaceToAdd, _provider.Workspaces.Find(_dbWorkspaceToAdd.Id));
-        }
-
-        #endregion
-
-        #region GetWorkspace
-        [Test]
-        public void ShouldReturnExistsWorkspace()
-        {
-            SerializerAssert.AreEqual(_repository.Get(_dbWorkspaceInDb.Id), _dbWorkspaceInDb);
-        }
-
-        [Test]
-        public void ShouldThrowNotFountExcWhenDoesNotExistWorkspaceWithThisId()
-        {
-            var incorrectId = Guid.NewGuid();
-
-            Assert.Throws<NotFoundException>(() => _repository.Get(incorrectId));
-        }
-        #endregion
-
-        #region SwitchActiveStatus
-        [Test]
-        public void ShouldSwitchActiveStatusSuccessfully()
-        {
-            _dbWorkspaceInDb.IsActive = true;
-
-            var id = _dbWorkspaceInDb.Id;
-
-            Assert.IsTrue(_repository.SwitchActiveStatus(id, false));
-            Assert.IsFalse(_provider.Workspaces.FirstOrDefault(w => w.Id == id).IsActive);
-        }
-
-        [Test]
-        public void ShouldThrowNotFountExcWhenTryingSwitchStatusOfNonExistsWorkspace()
-        {
-            var id = Guid.NewGuid();
-
-            Assert.Throws<NotFoundException>(() => _repository.SwitchActiveStatus(id, false));
-        }
-        #endregion
+      CreateMemoryDb();
     }
+
+    public void CreateMemoryDb()
+    {
+      var dbOptions = new DbContextOptionsBuilder<MessageServiceDbContext>()
+             .UseInMemoryDatabase(databaseName: "InMemoryDatabase")
+             .Options;
+      _provider = new MessageServiceDbContext(dbOptions);
+
+      IDictionary<object, object> _items = new Dictionary<object, object>();
+      _items.Add("UserId", _userId);
+
+      _accessorMock = new();
+
+      _accessorMock
+          .Setup(x => x.HttpContext.Items)
+          .Returns(_items);
+
+      _repository = new WorkspaceRepository(_provider, _accessorMock.Object);
+    }
+
+    [SetUp]
+    public void SetUp()
+    {
+      _provider.Workspaces.Add(_dbWorkspaceInDb);
+      _provider.Save();
+    }
+
+    [TearDown]
+    public void CleanDb()
+    {
+      if (_provider.IsInMemory())
+      {
+        _provider.EnsureDeleted();
+      }
+    }
+
+    #region AddWorkspace
+
+    [Test]
+    public void ShouldAddWorkspaceCorrectly()
+    {
+      _dbWorkspaceToAdd.Id = Guid.NewGuid();
+
+      _repository.Add(_dbWorkspaceToAdd);
+
+      Assert.AreEqual(_dbWorkspaceToAdd, _provider.Workspaces.Find(_dbWorkspaceToAdd.Id));
+    }
+
+    #endregion
+
+    #region GetWorkspace
+    [Test]
+    public void ShouldReturnExistsWorkspace()
+    {
+      SerializerAssert.AreEqual(_repository.Get(_dbWorkspaceInDb.Id), _dbWorkspaceInDb);
+    }
+
+    [Test]
+    public void ShouldThrowNotFountExcWhenDoesNotExistWorkspaceWithThisId()
+    {
+      var incorrectId = Guid.NewGuid();
+
+      Assert.Throws<NotFoundException>(() => _repository.Get(incorrectId));
+    }
+    #endregion
+
+    #region SwitchActiveStatus
+    [Test]
+    public void ShouldSwitchActiveStatusSuccessfully()
+    {
+      _dbWorkspaceInDb.IsActive = true;
+
+      var id = _dbWorkspaceInDb.Id;
+
+      Assert.IsTrue(_repository.SwitchActiveStatus(id, false));
+      Assert.IsFalse(_provider.Workspaces.FirstOrDefault(w => w.Id == id).IsActive);
+    }
+
+    [Test]
+    public void ShouldThrowNotFountExcWhenTryingSwitchStatusOfNonExistsWorkspace()
+    {
+      var id = Guid.NewGuid();
+
+      Assert.Throws<NotFoundException>(() => _repository.SwitchActiveStatus(id, false));
+    }
+    #endregion
+  }
 }
