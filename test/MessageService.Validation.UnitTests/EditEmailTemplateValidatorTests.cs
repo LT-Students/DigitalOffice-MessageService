0921023aeea43f174ddf483835bc38ba6c125b29
BelehovEgor <belehov.egor2001@yandex.ru>
2021-08-30 14:30:13 +0300

    fix test

--- test/MessageService.Validation.UnitTests/EditEmailTemplateValidatorTests.cs
+++ test/MessageService.Validation.UnitTests/EditEmailTemplateValidatorTests.cs
@@ -1,8 +1,7 @@
 ﻿using FluentValidation;
 using FluentValidation.TestHelper;
-using LT.DigitalOffice.MessageService.Models.Dto.Enums;
 using LT.DigitalOffice.MessageService.Models.Dto.Requests.EmailTemplate;
-using LT.DigitalOffice.MessageService.Validation.EmailTemplate;
+using LT.DigitalOffice.MessageService.Validation.Validators.EmailTemplate;
 using LT.DigitalOffice.Models.Broker.Enums;
 using NUnit.Framework;
 using System;
@@ -10,7 +9,7 @@ using System.Collections.Generic;
 
 namespace LT.DigitalOffice.MessageService.Validation.UnitTests
 {
-    public class EditEmailTemplateValidatorTests
+  public class EditEmailTemplateValidatorTests
     {
         private IValidator<EditEmailTemplateRequest> _validator;
         private EditEmailTemplateRequest _emailTemplateRequest;
