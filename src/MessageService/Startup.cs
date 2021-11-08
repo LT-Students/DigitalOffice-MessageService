dc29f8e8a27e4d433ef29f5615f0731ed6feab8f
Nikita <cheephot4@gmail.com>
2021-11-08 14:45:57 +0300

    romove create_message_endpoint and get_channel

--- src/MessageService/Startup.cs
+++ src/MessageService/Startup.cs
@@ -6,7 +6,6 @@ using LT.DigitalOffice.Kernel.Extensions;
 using LT.DigitalOffice.Kernel.Middlewares.ApiInformation;
 using LT.DigitalOffice.Kernel.Middlewares.Token;
 using LT.DigitalOffice.MessageService.Broker.Consumers;
-using LT.DigitalOffice.MessageService.Business.Commands.Message.Hubs;
 using LT.DigitalOffice.MessageService.Data.Provider.MsSql.Ef;
 using LT.DigitalOffice.MessageService.Models.Dto.Configurations;
 using MassTransit;
@@ -115,8 +114,6 @@ namespace LT.DigitalOffice.MessageService
       services.Configure<BaseRabbitMqConfig>(Configuration.GetSection(BaseRabbitMqConfig.SectionName));
       services.Configure<BaseServiceInfoConfig>(Configuration.GetSection(BaseServiceInfoConfig.SectionName));
 
-      services.AddSignalR();
-
       services.AddHttpContextAccessor();
 
       services.AddDbContext<MessageServiceDbContext>(options =>
@@ -156,8 +153,6 @@ namespace LT.DigitalOffice.MessageService
 
       app.UseEndpoints(endpoints =>
       {
-        endpoints.MapHub<ChatHub>("/chatHub");
-
         endpoints.MapControllers().RequireCors(CorsPolicyName);
 
         endpoints.MapHealthChecks($"/{_serviceInfoConfig.Id}/hc", new HealthCheckOptions
