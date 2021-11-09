95184a5f928fff7047aa322de108f97d889e6630
Nikita <cheephot4@gmail.com>
2021-11-09 13:23:15 +0300

    fix

--- src/MessageService/Controllers/MessageController.cs
+++ src/MessageService/Controllers/MessageController.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using LT.DigitalOffice.Kernel.Responses;
 using LT.DigitalOffice.MessageService.Business.Commands.Message.Interfaces;
+using LT.DigitalOffice.MessageService.Models.Dto.Enums;
 using LT.DigitalOffice.MessageService.Models.Dto.Requests.Message;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,7 +13,7 @@ namespace LT.DigitalOffice.MessageService.Controllers
   public class MessageController : ControllerBase
   {
     [HttpPost("create")]
-    public async Task<OperationResultResponse<Task>> CreateAsync(
+    public async Task<OperationResultResponse<StatusType>> CreateAsync(
       [FromServices] ICreateMessageCommand command,
       [FromBody] CreateMessageRequest request)
     {
