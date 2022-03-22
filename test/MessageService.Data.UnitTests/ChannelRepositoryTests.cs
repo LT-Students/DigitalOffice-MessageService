d355ab6f7c6ea376c6851b5a4202959bf493e0fc
BelehovEgor <ebelekhov@foss.dk>
2022-03-22 20:21:45 +0300

    fix

--- test/MessageService.Data.UnitTests/ChannelRepositoryTests.cs
+++ test/MessageService.Data.UnitTests/ChannelRepositoryTests.cs
@@ -1,34 +1,31 @@
 ﻿using LT.DigitalOffice.MessageService.Data.Interfaces;
 using LT.DigitalOffice.MessageService.Data.Provider;
 using LT.DigitalOffice.MessageService.Data.Provider.MsSql.Ef;
-using LT.DigitalOffice.MessageService.Models.Db;
 using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
-using System;
-using System.Linq;
 
 namespace LT.DigitalOffice.MessageService.Data.UnitTests
 {
-    public class ChannelRepositoryTests
+  public class ChannelRepositoryTests
     {
         private IChannelRepository _repository;
         private IDataProvider _provider;
 
-        [OneTimeSetUp]
-        public void OneTimeSetUp()
-        {
-            CreateMemoryDb();
-        }
+        //[OneTimeSetUp]
+        //public void OneTimeSetUp()
+        //{
+        //    CreateMemoryDb();
+        //}
 
-        public void CreateMemoryDb()
-        {
-            var dbOptions = new DbContextOptionsBuilder<MessageServiceDbContext>()
-                   .UseInMemoryDatabase(databaseName: "InMemoryDatabase")
-                   .Options;
-            _provider = new MessageServiceDbContext(dbOptions);
+        //public void CreateMemoryDb()
+        //{
+        //    var dbOptions = new DbContextOptionsBuilder<MessageServiceDbContext>()
+        //           .UseInMemoryDatabase(databaseName: "InMemoryDatabase")
+        //           .Options;
+        //    _provider = new MessageServiceDbContext(dbOptions);
 
-            _repository = new ChannelRepository(_provider);
-        }
+        //    _repository = new ChannelRepository(_provider);
+        //}
 
         /*[TearDown]
         public void CleanDb()
